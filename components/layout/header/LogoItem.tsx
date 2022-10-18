3b91a22718cec19f479d24181afaa18b23ccd03f
최영원 <youngwon0607@gmail.com>
2022-10-19 00:57:03 +0900

    fix : build 에러 해결
    - mui styled 사용시 타입스크립트 에러
    - emotion 플러그인 다시 추가

--- components/layout/header/LogoItem.tsx
+++ components/layout/header/LogoItem.tsx
@@ -4,14 +4,6 @@ import Box from "@mui/material/Box";
 import { MenuItem } from "@mui/material";
 import { Typography } from "@mui/material";
 import Link from "next/link";
-import { styled } from "@mui/system";
-
-const LogoText: typeof Typography = styled(Typography)({
-  marginLeft: { xs: "5px", sm: "10px" },
-  fontSize: { xs: "22px", sm: "25px" },
-  fontFamily: "Arial",
-  fontWeight: "bold",
-});
 
 const LogoItem: React.FC = () => {
   return (
@@ -26,7 +18,16 @@ const LogoItem: React.FC = () => {
       <Link href={"/"}>
         <MenuItem>
           <CodeIcon sx={{ fontSize: { xs: "35px", sm: "45px" } }} />
-          <LogoText>modernToujours.dev</LogoText>
+          <Typography
+            sx={{
+              fontFamily: "Arial",
+              fontWeight: "bold",
+              fontSize: { xs: "22px", sm: "25px" },
+              marginLeft: { xs: "5px", sm: "10px" },
+            }}
+          >
+            modernToujours.dev
+          </Typography>
         </MenuItem>
       </Link>
     </Box>
