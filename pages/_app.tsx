7f4eecc42d40be9ec629d5aea0bc8b41b6a77286
최영원 <youngwon0607@gmail.com>
2022-10-20 16:10:38 +0900

    fix : 높이 줄일시 컨텐츠 잘림 문제 해결
    min-height넣어서 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,35 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/layout/header/Header";
 import ColorModeContextProvider from "../context/ColorModeContextProvider";
+import { Box } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ColorModeContextProvider>
-      <Header />
-      <Component {...pageProps} />
+      <Box sx={{ overflow: "hidden" }}>
+        <Header />
+        <Box
+          sx={{
+            display: "block",
+            height: "70px",
+            visibility: "hidden",
+          }}
+        />
+        <Box
+          sx={{
+            width: "100%",
+            minHeight: "calc(100vh - 70px)",
+            display: "flex",
+            backgroundColor: "background.paper",
+            alignItems: "center",
+            textAlign: "center",
+            justifyContent: "center",
+            overflow: "scroll",
+          }}
+        >
+          <Component {...pageProps} />
+        </Box>
+      </Box>
     </ColorModeContextProvider>
   );
 }
