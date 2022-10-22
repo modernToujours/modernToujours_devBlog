abe4ead2140ffa45bfa5ca18d51da1d6bac59a00
최영원 <youngwon0607@gmail.com>
2022-10-22 16:13:10 +0900

    feature : 로그인 기능 추가

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -1,7 +1,16 @@
-import { hash } from "bcrypt";
+import { Password } from "@mui/icons-material";
+import { compare, hash } from "bcrypt";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const hashedPassword = await hash(password, 12);
 
   return hashedPassword;
+};
+
+export const verifyPassword = async (
+  password: string,
+  hashedPassword: string
+) => {
+  const isValid: boolean = await compare(password, hashedPassword);
+  return isValid;
 };
---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+
+import { connectDatabase } from "../../../lib/connect";
+import { verifyPassword } from "../../../lib/auth";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  type: string;
+};
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials: any, _req): Promise<User> => {
+        const client = await connectDatabase();
+        const db = client.db(process.env.mongodb_database);
+        const collection = db.collection("users");
+        const user = await collection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+          throw new Error("No user found!");
+        }
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Invalid password! Try again!");
+        }
+
+        const loginUser: User = {
+          id: user._id.toString(),
+          name: user.name,
+          email: user.email,
+          type: user.type,
+        };
+
+        client.close();
+
+        return loginUser;
+      },
+    }),
+  ],
+});
