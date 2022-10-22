f5a5590df470f0c2df6428b1d37d443045d65a2f
최영원 <youngwon0607@gmail.com>
2022-10-22 09:41:41 +0900

    feature : contact alert 메세지 추가

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,19 +1,43 @@
 import React, {
   ChangeEvent,
+  SyntheticEvent,
   ChangeEventHandler,
   MouseEventHandler,
   useState,
 } from "react";
 import axios from "axios";
 
-import { Divider, TextField, Typography, Button } from "@mui/material";
+import {
+  Divider,
+  TextField,
+  Typography,
+  Button,
+  Snackbar,
+  Alert,
+} from "@mui/material";
 
 import CardForm from "../layout/main/CardForm";
 
 const ContactForm: React.FC = () => {
+  const [openSuccess, setOpenSuccess] = useState<boolean>(false);
+  const [openFail, setOpenFail] = useState<boolean>(false);
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+
+  const closeAlert = (event: SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSuccess(false);
+  };
+
+  const closeFailAlert = (event: SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenFail(false);
+  };
 
   const inputNameHandler: ChangeEventHandler<HTMLInputElement> = (
     event: ChangeEvent<HTMLInputElement>
@@ -34,12 +58,15 @@ const ContactForm: React.FC = () => {
   };
 
   const submitContactHandler: MouseEventHandler<HTMLButtonElement> = () => {
-    console.log(name, email, message);
-    try {
-      axios.post("api/contact", { name, email, message });
-    } catch (error) {
-      console.log(error);
-    }
+    axios
+      .post("api/contact", { name, email, message })
+      .then((res) => {
+        setOpenSuccess(true);
+        console.log(res);
+      })
+      .catch((_) => {
+        setOpenFail(true);
+      });
   };
 
   return (
@@ -84,6 +111,16 @@ const ContactForm: React.FC = () => {
       >
         <Typography variant="h5">Submit</Typography>
       </Button>
+      <Snackbar open={openSuccess} autoHideDuration={3000}>
+        <Alert severity="success" sx={{ width: "100%" }} onClose={closeAlert}>
+          Success!
+        </Alert>
+      </Snackbar>
+      <Snackbar open={openFail} autoHideDuration={3000}>
+        <Alert severity="error" onClose={closeFailAlert} sx={{ width: "100%" }}>
+          입력란을 다시 확인해주세요!
+        </Alert>
+      </Snackbar>
     </CardForm>
   );
 };
--- lib/connect.tsx
+++ lib/connect.tsx
@@ -4,9 +4,8 @@ export const connectDatabase = async () => {
   const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.zbyacvk.mongodb.net/?retryWrites=true&w=majority`;
 
   const client: MongoClient = new MongoClient(connectionString);
-  console.log("1");
+
   await client.connect();
-  console.log("2");
 
   return client;
 };
