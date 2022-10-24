8535e0a13ad37469bbc02effcaa6f492bebe068d
최영원 <youngwon0607@gmail.com>
2022-10-24 11:07:23 +0900

    feature : google 및 s3 이미지 사용가능하도록 수정

--- next.config.js
+++ next.config.js
@@ -7,7 +7,12 @@ const nextConfig = {
     emotion: true,
   },
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "googleusercontent.com",
+      `${process.env.S3_UPLOAD_BUCKET}.${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+      `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
+    ],
   },
 };
 
