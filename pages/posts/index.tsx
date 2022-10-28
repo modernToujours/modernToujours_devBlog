af7e8d76dbd12527e24111e1f759f3f7c412a69c
최영원 <youngwon0607@gmail.com>
2022-10-28 13:48:02 +0900

    feature :  글 내용 불러오기

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
-import { GetStaticProps } from "next";
+import { useEffect, useState } from "react";
+import { GetStaticProps, NextPage } from "next";
 import AllPosts from "../../components/posts/AllPosts";
+import axios from "axios";
+import { ObjectId } from "mongodb";
 
-const AllPostsPage = () => {
-  return <AllPosts />;
+type post = { _id: ObjectId; title: string; image: string; post: string };
+
+const AllPostsPage: NextPage = () => {
+  const [posts, setPosts] = useState<post[]>([]);
+
+  useEffect(() => {
+    axios.get("/api/posts").then((res) => setPosts(res.data.posts));
+  }, []);
+  return <AllPosts posts={posts} />;
 };
 
 export default AllPostsPage;
 
 export const getStaticProps: GetStaticProps = async () => {
+  // const res = await axios.get("/api/posts");
+  // const posts = res.data.posts;
   return {
+    // props: { posts: posts },
     props: {},
     revalidate: 600,
   };
